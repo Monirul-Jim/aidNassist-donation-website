c82fd2d4aff8328e34a91619c9c9b6ec73482eac
sayeed083 <sayeedislam407@gmailcom>
2023-08-19 20:18:39 +0600

    Added

--- src/components/All_Volunteers/All_Volunteers.jsx
+++ src/components/All_Volunteers/All_Volunteers.jsx
@@ -6,7 +6,7 @@ const All_Volunteers = async () => {
     const volunteers = await getVolunteers();
     return (
         <div className="font-serif my-10">
-            <h2 className="text-3xl text-center">This is All Volunteers Page</h2>
+            <h2 className="text-3xl text-center"> All Volunteers</h2>
             <div className="grid grid-cols-3 gap-5 ">
                 {volunteers.map((person) => (
                     <One_Volunteer key={person._id} person={person}></One_Volunteer>
