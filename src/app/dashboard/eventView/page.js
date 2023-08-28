c544fabf9437e1c39115cc057ea19818282266c8
sayeed083 <sayeedislam407@gmailcom>
2023-08-28 19:34:18 +0600

    fixed firebase

--- src/app/dashboard/eventView/page.js
+++ src/app/dashboard/eventView/page.js
@@ -3,7 +3,7 @@
 const thisisView = () => {
     return (
         <div>
-            <h2 className="text-3xl text-center mt-0 md:mt-10 font-semibold">This is the Event Side View</h2>
+            <h2 className="text-3xl text-center mt-0 md:mt-10 font-semibold">This is the Event Side View S</h2>
         </div>
     );
 };
