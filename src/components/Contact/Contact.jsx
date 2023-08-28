927f9fc678a5f229ceb992f02101585d1147ed13
sayeed083 <sayeedislam407@gmailcom>
2023-08-28 23:59:21 +0600

    Updated Contact Feedback and Testimonials

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,59 +1,3 @@
-// 'use client'
-// import React, { useRef } from 'react';
-
-// import emailjs from '@emailjs/browser'
-// import { FaTelegramPlane } from 'react-icons/fa';
-// import Image from 'next/image';
-
-// import contactimage from '../../../public/Gallery/contact-us-main.gif'
-// import Swal from 'sweetalert2';
-
-// const Contact = () => {
-//     const form = useRef();
-//   const sendEmail = (e) => {
-//     e.preventDefault();
-
-//     emailjs.sendForm('service_qymata3', 'template_l9b11q2', form.current, '27auYAxRn2wcbS3p9')
-//       .then((result) => {
-//           console.log(result.text);
-//           Swal.fire(
-//             'Sent your Messege Successfully',
-//             'You clicked the button!',
-//             'success'
-//           )
-//           e.target.reset()
-//       }, (error) => {
-//           console.log(error.text);
-//       });
-//     }
-
-//     return (
-//        <div className='my-10'>
-//         <div ><h1 className="text-center text-5xl font-serif text-gray-600 mb-5">=== Contact Us for Any doubt or Questions ===</h1></div>
-//          <div className='lg:flex gap-20 justify-center items-center'>
-//             <div className='lg:w-[500px] w-96 h-96 lg:h-[500px] rounded-xl lg:mt-9'>
-        
-//             <Image width={520}  height={500} src={contactimage} alt=''></Image>
-//             </div>
-//          <div className='ms-10 lg:ms-0'>
-//   <form className='w-96 justify-center mt-3' ref={form} onSubmit={sendEmail}>
-//       <div className='font-bold text-green-400 mt-2'><label>Name</label></div>
-//       <div className=''><input className='w-72 border rounded-xl border-green-400  p-2'  type="text" name="user_name"/></div>
-//       <div className='font-bold text-green-400'><label>Email</label></div>
-//       <div><input className='w-72 border rounded-xl border-green-400  p-2 text-black'  type="email" name="user_email" /></div>
-//       <div className='font-bold text-green-400'><label>Message</label></div>
-//       <div><textarea className='w-72 h-48 border rounded-xl border-green-400  p-2'  name="message" /></div>
-//       <span className='bg-green-400 p-3 text-white rounded-xl mt-3 flex items-center  w-72 justify-center'><FaTelegramPlane /> <input className='ms-2'  type="submit" value="Send" /></span>
-//     </form>
-//       </div>
-//      </div>
-//        </div>
-//     );
-// };
-
-// export default Contact;
-
-
 "use client";
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
@@ -93,7 +37,7 @@ const Contact = () => {
 	return (
 		<section className="my-10">
 			<div>
-				<h1 className="text-center font-bold mt-20 pb-10 text-5xl text-gray-600 mb-5">
+				<h1 className="text-center font-bold mt-20 pb-10 text-5xl text-gray-600  mb-5">
 					Contact Us
 				</h1>
 			</div>
@@ -102,38 +46,40 @@ const Contact = () => {
 					<Image width={750} height={550} src={contactimage} alt="" />
 				</div>
 				<div className="lg:ms-10 md:ms-0 lg:w-[50%] md:w-[70%]">
-					<div className="bg-gray-50 border-2 p-8 rounded-md shadow-md w-[90%] mx-auto">
+					<div className="bg-gray-50 border-4 p-8 border-green-400  shadow-2xl w-[90%] mx-auto" style={{ borderRadius: '0px 55px 0px 55px' }}>
 						<form
 							className="w-[100%] justify-center mt-3"
 							ref={form}
 							onSubmit={sendEmail}
 						>
-							<div className="font-bold mt-2">
+							<div className="font-bold mt-2 dark:text-green-400">
 								<label>Name</label>
 							</div>
 							<div>
 								<input
-									className="w-full border rounded-md border-emerald-500 p-2"
+									className="w-full border rounded-md border-emerald-500 p-2 text-black"
 									type="text"
-									name="user_name"
+									name="from_name"
+									autocomplete="name"
 								/>
 							</div>
-							<div className="font-bold mt-3">
+							<div className="font-bold mt-3 dark:text-green-400">
 								<label>Email</label>
 							</div>
 							<div>
 								<input
 									className="w-full border rounded-md border-emerald-500 p-2 text-black"
 									type="email"
-									name="user_email"
+									name="from_email"
+                                    autocomplete="email"
 								/>
 							</div>
-							<div className="font-bold mt-3">
+							<div className="font-bold mt-3 dark:text-green-400 ">
 								<label>Message</label>
 							</div>
 							<div>
 								<textarea
-									className="w-full h-48 border rounded-md border-emerald-500 p-2"
+									className="w-full h-48 border rounded-md border-emerald-500 p-2 text-black"
 									name="message"
 								/>
 							</div>
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,96 +1,73 @@
-import React from "react";
-import "./testimonial.css";
-import Image from "next/image";
-import frist from "../../../public/Gallery/gallery-8.jpg";
+import React from 'react';
+import './testimonial.css'
+import Image from 'next/image';
+import frist from '../../../public/Gallery/testing.jpg'
 
 const Testimonial = () => {
-	return (
-		<div>
-			<h1 className="text-5xl font-bold mb-6 text-center mt-32 pb-4">
-				Testimonial
-			</h1>
-			<div class>
-
-			</div>
-			<div className="bannersetup bg-fixed pt-20 lg:h-[600px] h-[600] w-full">
-				<div className="lg:grid lg:grid-cols-2 max-w-5xl mx-auto">
-					{/* 1st */}
-					<div className="pt-2 lg:pt-0">
-						<div className="triangle absolute inset-0 opacity-40 z-10">
-							<h1 className="relative z-20 text-black font-extrabold">
-								The donation options are diverse, and I
+    return (
+        <div>
+            <h1 className='text-5xl font-serif text-gray-600 text-center my-3'>Testimonial</h1>
+            <div className='bannersetup bg-fixed font-serif  h-fit w-full py-3'>
+             <div className='lg:grid lg:grid-cols-2'>
+                {/* 1st */}
+                <div className='pt-2 lg:pt-0'>
+             <div className="triangle absolute inset-0 opacity-40 z-10"><h1 className='relative z-20 text-black font-extrabold'>The donation options are diverse, and I
 								appreciate the transparency in showing how the
-								funds are used
-							</h1>
-						</div>
-						<div className="flex justify-center items-center ">
-							<Image
-								className="w-14 h-14 rounded-full border-4 border-green-400"
-								src={frist}
-								alt="image"
-							/>
-						</div>
-					</div>
-					{/* 1st end */}
-					{/* 2nd */}
-					<div>
-						<div className="triangle absolute inset-0 opacity-40 z-10">
-							<h1 className="relative z-20 text-black font-extrabold">
-								I have been using this platform to donate for a
-								while now, and it has never disappointed me.
-								Highly recommended!
-							</h1>
-						</div>
-						<div className="flex justify-center items-center ">
-							<Image
-								className="w-14 h-14 rounded-full border-4 border-green-400"
-								src={frist}
-								alt="image"
-							/>
-						</div>
-					</div>
-					{/* 2nd end */}
-					{/* 3rd */}
-					<div>
-						<div className="triangle absolute inset-0 opacity-40 z-10">
-							<h1 className="relative z-20 text-black font-extrabold">
-								I am really impressed with how easy it was to
-								donate on this website. The process was smooth
-								and secure.
-							</h1>
-						</div>
-						<div className="flex justify-center items-center ">
-							<Image
-								className="w-14 h-14 rounded-full border-4 border-green-400"
-								src={frist}
-								alt="image"
-							/>
-						</div>
-					</div>
-					{/* 3rd end */}
-					{/* 4th */}
-					<div>
-						<div className="triangle absolute inset-0 opacity-40 z-10">
-							<h1 className="relative z-20 text-black font-extrabold">
-								I am really impressed with how easy it was to
-								donate on this website. The process was smooth
-								and secure.
-							</h1>
-						</div>
-						<div className="flex justify-center items-center ">
-							<Image
-								className="w-14 h-14 rounded-full border-4 border-green-400"
-								src={frist}
-								alt="image"
-							/>
-						</div>
-					</div>
-					{/* 4th end */}
-				</div>
-				
-			</div>
-		</div>
-	);
+								funds are used</h1></div>
+             <div className='flex justify-center items-center '>
+            <Image className='w-14 h-14 rounded-full border-4 border-green-400' src={frist} alt='image' />
+            </div>
+           </div>
+                {/* 1st end */}
+                 {/* 2nd */}
+                 <div>
+             <div className="triangle absolute inset-0 opacity-40 z-10"><h1 className='relative z-20 text-black font-extrabold'>The donation options are diverse, and I
+								appreciate the transparency in showing how the
+								funds are used</h1></div>
+             <div className='flex justify-center items-center '>
+            <Image className='w-14 h-14 rounded-full border-4 border-green-400' src={frist} alt='image' />
+            </div>
+           </div>
+                {/* 2nd end */}
+                 {/* 3rd */}
+                 <div>
+             <div className="triangle absolute inset-0 opacity-40 z-10"><h1 className='relative z-20 text-black font-extrabold'>The donation options are diverse, and I
+								appreciate the transparency in showing how the
+								funds are used</h1></div>
+             <div className='flex justify-center items-center '>
+            <Image className='w-14 h-14 rounded-full border-4 border-green-400' src={frist} alt='image' />
+            </div>
+           </div>
+                {/* 3rd end */}
+                {/* 4th */}
+                <div>
+             <div className="triangle absolute inset-0 opacity-40 z-10"><h1 className='relative z-20 text-black font-extrabold'>The donation options are diverse, and I
+								appreciate the transparency in showing how the
+								funds are used</h1></div>
+             <div className='flex justify-center items-center '>
+            <Image className='w-14 h-14 rounded-full border-4 border-green-400' src={frist} alt='image' />
+            </div>
+           </div>
+                {/* 4th end */}
+             </div>
+             {/* after two cols */}
+
+             {/* 5th */}
+            <div className='lg:flex lg:justify-center pb-3 lg:pb-0'>
+            <div>
+             <div className="triangle absolute inset-0 opacity-40 z-10"><h1 className='relative z-20 text-black font-extrabold'>The donation options are diverse, and I
+								appreciate the transparency in showing how the
+								funds are used</h1></div>
+             <div className='flex justify-center items-center '>
+            <Image className='w-14 h-14 rounded-full border-4 border-green-400' src={frist} alt='image' />
+            </div>
+           </div>
+            </div>
+                {/* 5th end */}
+
+        </div>
+        </div>
+    );
 };
 
 export default Testimonial;
