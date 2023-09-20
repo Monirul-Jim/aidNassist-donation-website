13d17e543e12201885468595e1ef5ebb24c60dda
sayeed083 <sayeedislam407@gmailcom>
2023-09-20 17:38:35 +0600

    Added JWT

---
+++ src/app/api/auth/logout/route.js
@@ -0,0 +1,17 @@
+
+import { NextResponse } from "next/server";
+
+export const POST = async (request) => {
+
+    const res = new NextResponse(JSON.stringify({
+        message: "Logout Successfully"
+    })
+    );
+    res.cookies.set("jwt_secret", "", {
+        expires: new Date(Date.now())
+    });
+
+    return res;
+
+
+}
